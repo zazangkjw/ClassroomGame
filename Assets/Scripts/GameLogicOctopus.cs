3e9b7d6f44e932989f1735c935355c8384cbab95
zazangkjw <zazangkjw@naver.com>
2025-05-27 05:23:08 +0900

    캐릭터 시작

--- Assets/Scripts/GameLogicOctopus.cs
+++ Assets/Scripts/GameLogicOctopus.cs
@@ -1,11 +1,6 @@
 using Fusion;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.Video;
 
 public class GameLogicOctopus : NetworkBehaviour, IPlayerJoined, IPlayerLeft
 {
